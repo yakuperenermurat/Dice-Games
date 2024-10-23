2df0d5cee1f9697537371ca74fc4754fcf2aef25
yakuperenermurat <yakuperenermurat@gmail.com>
2024-10-23 21:35:55 +0300

    updatess

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]',
+      },
+    },
+  },
+};
